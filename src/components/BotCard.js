8730d2623318c6c1034ca36e8c136131a9e481d1
hibby <hibaqku7@gmail.com>
2025-04-20 11:35:30 +0300

    Added BotCard component with onClick and OnDischarge handlers

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -4,6 +4,18 @@ function BotCard ({bot, onClick, OnDischarge}) {
     return(
         <div className="bot-card" onClick={() => onClick(bot)}>
             <img src={bot.avatar_url} alt={bot.name} />
+            <h2>{bot.name}</h2>
+            <p>{bot.bot_class}</p>
+            <p>{bot.catchphrase}</p>
+            <p>{bot.armor}</p>
+            <p>{bot.damage}</p>
+            <button onClick={(e) => {
+                e.stopPropagation();
+                OnDischarge(bot.id);
+                 }}>
+                     </button>
+
         </div>
     )
 }
+export default BotCard;
